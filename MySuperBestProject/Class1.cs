a3603514b1dc167194ea4959b4eca682f347d2b3
4teeen <artmvashunin14@gmail.com>
2022-02-22 16:14:08 +0300

    Добавьте файлы проекта.

---
+++ MySuperBestProject/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MySuperBestProject
+{
+    public static class Poww
+    {
+        public static void Powy(double x, double N)
+        {
+            x = Convert.ToDouble(Console.ReadLine());
+            N = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Pow(x, N));
+        }
+    }
+}
---
+++ MySuperBestProject/MySuperBestProject.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySuperBestProject;
+
+namespace MySuperBestProject.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
